fbd5527f3800500db42bc9a56575705128713edc
Alimzhan Zhandildauly <alimzanzandilda@gmail.com>
2024-01-12 22:42:56 +0600

    modified main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-
-def add_two_lists(list1: list, list2: list) -> list:
-    return list(map(lambda x,y: x + y, list1,list2))
-
-
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-result = add_two_lists(list1,list2)
+print('hello')
